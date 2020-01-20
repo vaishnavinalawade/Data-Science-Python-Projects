c2c45c528315c36cd18e9171bee08ecd6dd3b588
Vaishnavi Nalawade <vaish.nalawade7@gmail.com>
2020-01-20 07:57:04 +0530

    Machine Learning A-Z
    Data Preprocessing

---
+++ Machine Learning A-Z Template/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data-preprocessing.py
@@ -0,0 +1,17 @@
+#Data Preprocessing
+
+import numpy as np;
+import matplotlib.pyplot as plt;
+import pandas as pd;
+
+dataset = pd.read_csv('Data.csv');
+X = dataset.iloc[:,:-1].values;
+y = dataset.iloc[:,-1].values;
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
+
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
